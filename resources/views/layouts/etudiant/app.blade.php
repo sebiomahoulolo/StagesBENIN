27a602eff3c54e7f490549abdf96ce59a6f45db5
SaliouAboudou <105776499+SaliouAboudou@users.noreply.github.com>
2025-06-02 18:01:35 +0100

    add

--- resources/views/layouts/etudiant/app.blade.php
+++ resources/views/layouts/etudiant/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,8 +16,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+
     @livewireStyles
     @stack('styles')
 
@@ -24,10 +26,9 @@
     <script src="//unpkg.com/alpinejs" defer></script>
 </head>
 {{-- Initialise Alpine et gère le blocage du scroll --}}
-<body x-data="{ isSidebarOpen: false }"
-      {{-- Bloque le scroll du body uniquement quand la sidebar est ouverte ET qu'on est sur un petit écran --}}
-      :class="{ 'overflow-hidden': isSidebarOpen && window.innerWidth < 992 }"
-      class="@stack('body-class')">
+
+<body x-data="{ isSidebarOpen: false }" {{-- Bloque le scroll du body uniquement quand la sidebar est ouverte ET qu'on est sur un petit écran --}}
+    :class="{ 'overflow-hidden': isSidebarOpen && window.innerWidth < 992 }" class="@stack('body-class')">
 
     @include('layouts.etudiant.partials.header')
 
@@ -41,20 +42,22 @@
     </div>
 
     {{-- Overlay (couvre le contenu quand la sidebar est ouverte sur petit écran) --}}
-    <div class="sidebar-overlay"
-         x-show="isSidebarOpen"
-         x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-         x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-         @click="isSidebarOpen = false"
-         x-cloak {{-- Important --}}
-         style="display: none;"
-    ></div>
+    <div class="sidebar-overlay" x-show="isSidebarOpen" x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+        x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0" @click="isSidebarOpen = false" x-cloak {{-- Important --}}
+        style="display: none;"></div>
     {{-- <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script> --}}
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js" integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous"></script>
-
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js"
+        integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D" crossorigin="anonymous">
+    </script>
     
+
     @livewireScripts
     @stack('scripts')
 </body>
+
 </html>
